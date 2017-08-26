9e3d8094e94ec87b818140dd96646f76b15296f8
f.alfiansyah <alfianzach@gmail.com>
2017-08-27 06:18:07 +0700

    add financial checkup

--- application/views/pages/financialcheckup.php
+++ application/views/pages/financialcheckup.php
@@ -8,28 +8,83 @@
       <div class="panel-body">
         <h5>Income Statement</h5>
         <div class="form-group">
-          <label for="exampleInputPassword1" class="col-sm-2 control-label" >Penghasilan Per Bulan</label>
-          <div class="col-sm-10"></div>
-          <input type="text" class="form-control" id="penghasilanperbulan" placeholder="100.000">
+          <label for="exampleInputPassword1" class="col-md-2 control-label" >Penghasilan Per Bulan</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="penghasilanperbulan" placeholder="100.000">
+          </div>
         </div>
         <h5>Pengeluaran Per Bulan</h5>
         <div class="form-group">
-          <label for="exampleInputPassword1">Pendidikan</label>
-          <input type="text" class="form-control" id="pendidikan" placeholder="100.000">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label">Pendidikan</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="pendidikan" placeholder="100.000">
+          </div>
+          
         </div>
         <div class="form-group">
-          <label for="exampleInputPassword1">Kebutuhan Harian</label>
-          <input type="text" class="form-control" id="kebutuhanharian" placeholder="100.000">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label">Kebutuhan Harian</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="kebutuhanharian" placeholder="100.000">
+          </div>
+          
         </div>
         <div class="form-group">
-          <label for="exampleInputPassword1">Kebutuhan Rumah</label>
-          <input type="text" class="form-control" id="kebutuhanrumah" placeholder="100.000">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label">Kebutuhan Rumah</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="kebutuhanrumah" placeholder="100.000">
+          </div>
         </div>
         <div class="form-group">
-          <label for="exampleInputPassword1">Lifestyle</label>
-          <input type="text" class="form-control" id="lifestyle" placeholder="100.000">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label">Lifestyle</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="lifestyle" placeholder="100.000">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label">Total</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="total" placeholder="100.000">
+          </div>
+        </div>
+        <h5>Pengeluaran Cicilan</h5>
+        <div class="form-group">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label"> Rumah</label>
+          <label class="col-sm-4 control-label"> Sisa Term <input type="text" name="term1">  bulan</label>
+          <label class="col-sm-6 control-label"> Besar Cicilan <input type="text" name="cicilan1"></label>
+          
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label"> Kendaraan</label>
+          <label class="col-sm-4 control-label"> Sisa Term <input type="text" name="term2">  bulan</label>
+          <label class="col-sm-6 control-label"> Besar Cicilan <input type="text" name="cicilan2"></label>
+          
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label"> Gadget</label>
+          <label class="col-sm-4 control-label"> Sisa Term <input type="text" name="term3">  bulan</label>
+          <label class="col-sm-6 control-label"> Besar Cicilan <input type="text" name="cicilan3"></label>
+          
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label" >Tabungan Per Bulan</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="tabunganperbulan" placeholder="100.000">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label" >Penghasilan Bersih</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="penghasilanbersih" placeholder="100.000">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1" class="col-sm-2 control-label" >Saldo Tabungan Saat Ini</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" name="saldotabungan" placeholder="100.000">
+          </div>
         </div>
       </div>
+
       <!-- /.box-body -->
 
       <div class="box-footer">
